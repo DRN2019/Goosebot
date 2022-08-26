7232e25b833e4fd5e81d5a3f1d095f37b2dad3ab
DRN2019 <darrenhoho@hotmail.com>
2022-08-26 16:16:57 -0400

    Added a sql file to help set up a mySQL server, still need voice perms table

---
+++ server.sql
@@ -0,0 +1,19 @@
+CREATE DATABASE IF NOT EXISTS goosebot;
+
+CREATE TABLE servers IF NOT EXISTS(
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    guild_id INT NOT NULL,
+    guild_name varchar(33) NOT NULL
+);
+
+CREATE TABLE IF NOT EXISTS text_permissions (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    message_honk BOOLEAN NOT NULL DEFAULT 0,
+    reaction BOOLEAN NOT NULL DEFAULT 0,
+    memes BOOLEAN NOT NULL DEFAULT 0,
+    delete_msg BOOLEAN NOT NULL DEFAULT 0,
+    message_goose BOOLEAN NOT NULL DEFAULT 0,
+    no_peace BOOLEAN NOT NULL DEFAULT 0,
+    server_id INT NOT NULL,
+    FOREIGN KEY (server_id) REFERENCES servers(id)
+);
