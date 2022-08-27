7b5c1a9d6ad7b4c1d0058f74f032ab89d4373e39
DRN2019 <darrenhoho@hotmail.com>
2022-08-27 06:25:31 -0400

    Commit test

--- server.sql
+++ server.sql
@@ -3,7 +3,7 @@ CREATE DATABASE IF NOT EXISTS goosebot;
 CREATE TABLE servers IF NOT EXISTS(
     id INT AUTO_INCREMENT PRIMARY KEY,
     guild_id INT NOT NULL,
-    guild_name varchar(33) NOT NULL
+    guild_name varchar(33) NOT NULL,
 );
 
 CREATE TABLE IF NOT EXISTS text_permissions (
@@ -17,3 +17,11 @@ CREATE TABLE IF NOT EXISTS text_permissions (
     server_id INT NOT NULL,
     FOREIGN KEY (server_id) REFERENCES servers(id)
 );
+
+CREATE TABLE IF NOT EXISTS voice_permissions (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    disconnect BOOLEAN NOT NULL DEFAULT 0,
+    voice_honk BOOLEAN NOT NULL DEFAULT 0,
+    server_id INT,
+    FOREIGN KEY (server_id) REFERENCES servers(id)
+)
